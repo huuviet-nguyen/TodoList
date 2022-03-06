dc5f1cd57c827ff8002509198d8aa9c60dfe77dd
Bi <bi@imac-de-bi.home>
2022-03-06 15:01:00 +0100

    Init

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,19 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Bi on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+        }
+    }
+}
